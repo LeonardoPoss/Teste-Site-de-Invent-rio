ae646da3189a4de623886efcba52005abe06bef6
LeonardoPoss <Leonardoposs99@gmail.ccom>
2024-10-01 15:10:58 -0300

    -feat : Adição de arquivos referentes aos models, inciamentee apenas o de usuário. Criadas as propriedades getters e setters

---
+++ Server/Models/user.py
@@ -0,0 +1,41 @@
+class Usuario:
+    __nome =  str
+    __senha = str
+    __email = str
+    __cargo = str
+
+    def __init__(self, nome, senha, cargo):
+        self.__nome = nome 
+        self.__senha = senha
+        self.__cargo = cargo
+
+    @property
+    def nome (self):
+        return self.__nome
+    @nome.setter
+    def nome (self, nome):
+        self.__nome = nome
+
+    @property
+    def senha(self):
+        return 
+
+    @senha.setter
+    def senha (self, senha):
+        self.__senha = senha
+
+    @property
+    def email (self):
+        return self.__email
+    
+    @email.setter
+    def email (self, email):
+        self.__email = email
+
+    @property
+    def cargo (self):
+        return self.__cargo
+    
+    @cargo.setter
+    def cargo (self, cargo):
+        self.__cargo = cargo
